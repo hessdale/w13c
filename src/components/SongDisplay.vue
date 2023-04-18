31092660b71a29f829f48ed9372fb37c60155253
dale <hessdale96@gmail.com>
2023-04-18 16:02:36 -0600

    added variables for songs and simple html

---
+++ src/components/SongDisplay.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <section>
+      <h1>Now playing:{{ current.title }}</h1>
+    </section>
+    <section>
+      <h1>Playlist</h1>
+      <button v-for="song in songs" :key="song[`id`]">
+        {{ song[`title`] }} - {{ song[`artist`] }}
+      </button>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: {},
+      index: 0,
+      songs: [
+        {
+          title: `song1`,
+          artist: `artist1`,
+        },
+        {
+          title: `song2`,
+          artist: `artist2`,
+        },
+        {
+          title: `song3`,
+          artist: `artist3`,
+        },
+        {
+          title: `song4`,
+          artist: `artist4`,
+        },
+        {
+          title: `song5`,
+          artist: `artist5`,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
