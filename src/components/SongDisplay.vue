144e34bc2fd714e361de72f8ba0609f49f176fd6
dale <hessdale96@gmail.com>
2023-04-18 22:26:18 -0600

    added comments

--- src/components/SongDisplay.vue
+++ src/components/SongDisplay.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
     <section>
-      <h1>Now playing:{{ current.title }}</h1>
+      <!-- using the variable is playing with an if statement to display the current playing song or ask user to play a song -->
+      <div v-if="isPlaying != false">
+        <h1>Now playing: {{ current.title }} by {{ current.artist }}</h1>
+        <!-- using bind to get src from js current variable -->
+        <img :src="current.image_url" alt="album art" height="250px" />
+      </div>
+      <!-- else it displays html -->
+      <h1 v-else>Please play a song</h1>
     </section>
     <section>
       <h1>Playlist</h1>
-      <button v-for="song in songs" :key="song[`id`]">
-        {{ song[`title`] }} - {{ song[`artist`] }}
-      </button>
+      <!-- using a loop to add the buttons in to play a song -->
+      <div v-for="song in songs" :key="song[`id`]">
+        <!-- event listener to each of the buttons -->
+        <button @click="play(song)">
+          {{ song[`title`] }} - {{ song[`artist`] }}
+        </button>
+      </div>
     </section>
   </div>
 </template>
@@ -16,31 +27,52 @@
 export default {
   data() {
     return {
+      //empty current variable for song data to be stored in
       current: {},
-      index: 0,
+      //setting isPlaying variable to false on default
+      isPlaying: false,
+      //array of objects that hold all of the songs data (random generated words) and random pics
       songs: [
         {
-          title: `song1`,
-          artist: `artist1`,
+          title: `rare`,
+          artist: `new spell`,
+          song_id: 32196,
+          image_url: `https://images.pexels.com/photos/14844814/pexels-photo-14844814.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load`,
         },
         {
-          title: `song2`,
-          artist: `artist2`,
+          title: `expand`,
+          artist: `energy patrol`,
+          song_id: 35436,
+          image_url: `https://images.pexels.com/photos/10815995/pexels-photo-10815995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
         },
         {
-          title: `song3`,
-          artist: `artist3`,
+          title: `break`,
+          artist: `funeral`,
+          song_id: 31966,
+          image_url: `https://images.pexels.com/photos/14691039/pexels-photo-14691039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
         },
         {
-          title: `song4`,
-          artist: `artist4`,
+          title: `foster mayor`,
+          artist: `cheap alchohol`,
+          song_id: 36346,
+          image_url: `https://images.pexels.com/photos/15331169/pexels-photo-15331169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
         },
         {
-          title: `song5`,
-          artist: `artist5`,
+          title: `tooth`,
+          artist: `jet`,
+          song_id: 32356,
+          image_url: `https://images.pexels.com/photos/14711412/pexels-photo-14711412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
         },
       ],
     };
+  },
+  //methods for defining function when user clicks button to play
+  methods: {
+    play(song) {
+      //sets current to song info and sets is playing to true
+      this.current = song;
+      this.isPlaying = true;
+    },
   },
 };
 </script>
